e5a52e918236ccae692ec871a60a4af986217ca0
Jermaine Oppong <jeropp00@gmail.com>
2014-06-29 16:03:50 +0100

    Code cleanup. Created tictactoe object literal under window global with game functionality

--- index.js
+++ index.js
@@ -1,132 +1,180 @@
-// Global Variables used throughout code
-var player = 1; // will be either 1 or 2
-var tiles = document.querySelectorAll('input[type="button"]');
-var winner = false;
+window.tictactoe = {
 
-// Loops over all clickable tiles
-for (var i = 0; i < tiles.length; i++ ) {
+	player: 1,
 
-	tiles[i].addEventListener('click', function(clickEvent) { 
-		
-		var clickedTile = clickEvent.target;
+	tiles: $('.ttt-tiles'),
 
-		// Sets clicked state...
-		// ...passes turn to next player 
-		// ...and checks for winning combination
-		activateTile(clickedTile); 
-	});
-}
+	resetButton: $('.ttt-stage-reset-button'),
 
+	isWinner: false,
 
-// Runs functionality when tile is clicked
-function activateTile(tile) {
+	init: function () {
+			
+		var self = this;
 
-	if( player == 1) {
-		// player is 1, set player1 token
-		tile.value = 'X';
+		// Loops over all clickable tiles
+		this.tiles.each(function(index, tile) {
 
-		// change to next player
-		player += 1;
+			var $tile = $(tile);
 
-		// disable tile to prevent changing state
-		tile.disabled = 'disabled';
-		
-		// check for winning combinations
-		checkWinner('X');	
-	} else {
-		// player is 2, set player2 token
-		tile.value = 'O';
+			$tile.on('click', function(clickEvent) {
+				
+				// Sets clicked state...
+				// ...passes turn to next player 
+				// ...and checks for winning combination
+				self.activateClickedTile(clickEvent.target);
 
-		// change to previous player
-		player -= 1;
+			});
+		});
 
-		// disable tile to prevent overwriting state
-		tile.disabled = 'disabled';
+		// Set game reset event
+		this.createGameResetEvent();
 
-		// check for winning combinations
-		checkWinner('O');
-	}
+		// Capture tab events
+		this.captureTabEvents();
+	},
 
-	// Check for a draw! In other words no winners.
-	checkForDraw();
-}
+	activateClickedTile: function (clickedTile) {
 
+		if (this.player === 1) {
 
-// Check if there are any winning combinations then end game
-function checkWinner(playerToken) {
+			// player is 1, set player1 token
+			clickedTile.value = 'X';
 
-	// Store winning combinations...
-	var winningCombinations = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]];
+			// disable clickedTile to prevent changing state
+			clickedTile.disabled = 'disabled';
+			
+			// check for winning combinations
+			this.checkWinner('X');	
 
-	// ...and loop over the combination for matches
-	for (var i = 0; i < winningCombinations.length; i++) {
+			// change to next player
+			this.player += 1;
 
-		if( tiles[winningCombinations[i][0] - 1].value == playerToken && 
-			tiles[winningCombinations[i][1] - 1].value == playerToken &&
-			tiles[winningCombinations[i][2] - 1].value == playerToken ) {
+		} else if (this.player === 2) {
 
-			alert('the winner is player --' + playerToken + '--');
+			// player is 2, set player2 token
+			clickedTile.value = 'O';
 
-			disableAllTiles();
+			// disable clickedTile to prevent overwriting state
+			clickedTile.disabled = 'disabled';
 
-			// Set winner to true
-			winner = true;
+			// check for winning combinations
+			this.checkWinner('O');
 
-			console.log('Winning combination', winningCombinations[i][0], winningCombinations[i][1], winningCombinations[i][2]);
-
+			// change to previous player
+			this.player -= 1;
 		}
 
+
+		this.checkForDraw();
+
+	},
+
+	checkWinner: function (playerToken) {
+
+		var self = this;
+
+		// Store winning combinations...
+		var winningCombinations = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]];
+
+		$.each(winningCombinations, function (index, winningCombination) {
+
+			if( self.tiles.get(winningCombination[0] - 1).value == playerToken && 
+				self.tiles.get(winningCombination[1] - 1).value == playerToken &&
+				self.tiles.get(winningCombination[2] - 1).value == playerToken ) {
+
+				alert('the winner is player ' + self.player);
+
+				self.disableAllTiles();
+
+				// Set winner to true
+				self.isWinner = true;
+
+				console.log('Winning combination', winningCombination[0], winningCombination[1], winningCombination[2]);
+
+			}
+
+
+		});
+
+	},
+
+	checkForDraw: function() {
+
+		var filledTiles = 0;
+
+		// Loops over tiles and checked if they are all filled
+		this.tiles.each(function(index, tile) {
+			// If so and there is no win then it may be a draw
+			if(tile.value) filledTiles++;
+		});
+
+		if(filledTiles === this.tiles.length && !this.isWinner) {
+
+			alert('Its a draw!');
+
+			console.log('draw');
+		};
+	},
+
+	disableAllTiles: function () {
+
+		// Disable all tiles after a player wins
+		// to prevent further interaction with tiles
+		// until game is reset of course :)
+		this.tiles.each(function(index, tile) {
+
+			if(tile.value) return; // Ignore tiles already clicked
+			
+			// Disable unclicked tiles
+			tile.disabled = 'disabled';
+
+		});
+
+	},
+
+	createGameResetEvent: function() {
+
+		var self = this;
+
+		// Reset Button Functionality
+		self.resetButton.on('click', function() {
+
+			// Reset state of all tiles
+			// // Start a new game
+			self.tiles.each(function(index, tile) {
+
+				tile.value = '';
+				tile.disabled = '';
+
+			});
+
+			// Reset winner to false
+			self.isWinner = false;
+
+			// Re-enable to ensure that player 1(X) always goes first
+			//self.player = 1;
+
+		});
+
+	},
+
+	captureTabEvents: function () {
+
+		this.tiles.get(0).focus();
+
+		$(document).on('keydown', function(keydownEvent){
+
+			var characterCode = (typeof keydownEvent.which == 'undefined') ? keydownEvent.keyCode : keydownEvent.which;
+
+			if(characterCode === 9) { // If its tabbing key
+				keydownEvent.preventDefault();
+			}
+
+		});
 	}
-}
 
+};
 
-function checkForDraw() {
-	var filledTiles = 0;
-
-	// Loops over tiles and checked if they are all filled
-	for (var i = 0; i < tiles.length; i++) {
-		// If so and there is no win then it may be a draw
-		if(tiles[i].value) filledTiles++;
-	}
-
-	if(filledTiles == tiles.length && !winner) {
-
-		alert('Its a draw!');
-
-		console.log('draw');
-	};
-}
-
-// Disable all tiles after a player wins
-// to prevent further interaction with tiles
-// until game is reset of course :)
-function disableAllTiles() {
-
-	for (var i = 0; i < tiles.length; i++ ) {
-		if(tiles[i].value) continue; // Ignore tiles already clicked
-
-		// Disable unclicked tiles
-		tiles[i].disabled = 'disabled';
-	}
-
-}
-
-// Reset Button Functionality
-document.querySelector('.ttt-stage-reset-button').addEventListener('click',reset);
-
-// Reset All tiles
-function reset() {
-
-	// Reset state of all tiles
-	// // Start a new game
-	for (var i = 0; i < tiles.length; i++ ) {
-		tiles[i].value = '';
-		tiles[i].disabled = '';
-	}
-
-	// Reset winner to false
-	winner = false;
-
-	// Re-enable to ensure that player 1(X) always goes first
-	//player = 1;
-}
+// Initialize game
+tictactoe.init();
